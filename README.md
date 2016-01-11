086d5c1975d792a40968bdc61e954b689a903baf
TheMickeyMike <macnow@st.amu.edu.pl>
2016-01-11 18:01:41 +0100

    Create project

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # QA-Dates
 Ask me for birthdate of famous person
+ ## Libraries
+ [GitHub](https://github.com/goldsmith/Wikipedia) A Pythonic wrapper for the Wikipedia API https://wikipedia.readthedocs.org/
---
+++ main.py
@@ -0,0 +1,42 @@
+import wikipedia
+import re
+import sys
+
+
+pattern_pl = "ur. (([0-9]{1,2}\D*[0-9]{4}.*?)(,.zm|\)))"
+pattern_pl_2 = "ur. ([0-9]{1,2}\D*[0-9]{4}.*?)(\) –|-)"
+name = input("Kiedy urodził się ")
+
+# Let's try in polish, maybe famous?
+print (wikipedia.languages()['pl'])
+wikipedia.set_lang("pl")
+
+# result_pl = wikipedia.summary(name)
+# print(result_pl)
+# print(result.group(1).strip())
+
+result_pl = ""
+result_en = ""
+try:
+    result_pl = wikipedia.summary(name)
+except wikipedia.exceptions.DisambiguationError as e:
+    try:
+        # Let's try in eng
+        wikipedia.set_lang("en")
+        result_en = wikipedia.summary(name)
+    except wikipedia.exceptions.DisambiguationError as ee:
+        print (e.options)
+        print(ee.options)
+        sys.exit("Search Error!")
+if result_pl != "":
+    result = re.search(pattern_pl_2,result_pl)
+    # print(result_pl)
+    print(result.group(1))
+elif result_en != "":
+    result = re.search(pattern_pl,result_pl) #TODO Tu inny pattern
+    print(result.group(1))
+
+# print (wikipedia.summary(name))
+
+re.search(pattern_pl,result_pl)
+# print (wikipedia.search("Madonna (entertainer)"))
